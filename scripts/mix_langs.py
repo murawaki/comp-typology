895bc77f426f9342a40be7345a6333d8d0c42b0d
MURAWAKI Yugo <murawaki@ait.kyushu-u.ac.jp>
2015-10-30 12:33:17 +0900

    mix_langs.py: switched to new json format

--- scripts/mix_langs.py
+++ scripts/mix_langs.py
@@ -8,13 +8,13 @@ import numpy as np
 from argparse import ArgumentParser
 
 from rand_utils import rand_partition
-from json_utils import load_json_file
+from json_utils import load_json_file, load_json_stream
 from evaluator import CategoricalFeatureListEvaluator
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("--src", dest="src", metavar="FILE", default="khm")
-    parser.add_argument("--dst", dest="dst", metavar="FILE", default="khr")
+    parser.add_argument("--src", dest="src", metavar="WALS_CODE", default="khm")
+    parser.add_argument("--dst", dest="dst", metavar="WALS_CODE", default="khr")
     parser.add_argument("--ssize", dest="ssize", metavar="INT", type=int, default=100)
     parser.add_argument("--samples", dest="samples", metavar="INT", type=int, default=1000)
     parser.add_argument("--cat", dest="is_hid", action="store_false", default=True,
@@ -23,20 +23,22 @@ def main():
                         help="mixtures of hidden features (linear interpolation)")
     parser.add_argument("--auto", dest="is_auto", action="store_true", default=False,
                         help="adjust hidden features")
-    parser.add_argument("model", metavar="FILE", default=None)
-    parser.add_argument("langs", metavar="FILE", default=None)
+    parser.add_argument("model", metavar="MODEL", default=None)
+    parser.add_argument("langs", metavar="LANGS", default=None)
     args = parser.parse_args()
 
     with codecs.getreader("utf-8")(open(args.model)) as reader:
         dat = reader.read()
         evaluator = CategoricalFeatureListEvaluator.loads(dat)
     fid2struct = evaluator.fid2struct
-    langlist = load_json_file(args.langs)
+    wals_code2lang = {}
+    for lang in load_json_stream(open(args.langs)):
+        wals_code2lang[lang["wals_code"]] = lang
 
     # khm: Khmer
     # khr: Munda
-    k = np.array(langlist[args.src], dtype=np.int32)
-    m = np.array(langlist[args.dst], dtype=np.int32)
+    k = np.array(wals_code2lang[args.src]["catvect_filled"], dtype=np.int32)
+    m = np.array(wals_code2lang[args.dst]["catvect_filled"], dtype=np.int32)
 
     if args.is_hid:
         kh = evaluator.encode(evaluator.cat2bin(k))
